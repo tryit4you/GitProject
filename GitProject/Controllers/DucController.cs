0e80e3776191d0b8c939f6231e5afb3e536e97f1
Phạm Minh Đức <42537338+pmduc97@users.noreply.github.com>
2019-01-15 14:50:47 +0700

    Create DucController

---
+++ GitProject/Controllers/DucController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitProject.Controllers
+{
+    public class DucController : Controller
+    {
+        // GET: Duc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
