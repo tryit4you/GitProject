98a75b6ce532dbf876b04c14b203357f7ba52611
Phạm Minh Đức <42537338+pmduc97@users.noreply.github.com>
2019-01-15 15:14:22 +0700

    New Action Login

--- GitProject/Controllers/DucController.cs
+++ GitProject/Controllers/DucController.cs
@@ -13,5 +13,10 @@ namespace GitProject.Controllers
         {
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
     }
 }
