72cc10d38faa49d700f47bcc9f7f7d13d199d604
Phạm Minh Đức <42537338+pmduc97@users.noreply.github.com>
2019-01-15 14:26:37 +0700

    Đức Helloword

---
+++ GitProject/Controllers/HelloWordController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitProject.Controllers
+{
+    public class HelloWordController : Controller
+    {
+        // GET: HelloWord
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
